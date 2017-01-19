3cc16776a22fa1c996a0ea6db3b7e4633d413605
viktor4006094 <viktor.enfeldt@gmail.com>
2017-01-19 13:23:29 +0100

    commented out diffuse lighting from fragment shader

--- Fragment.hlsl
+++ Fragment.hlsl
@@ -12,14 +12,14 @@ struct VS_OUT
 
 float4 PS_main(VS_OUT input) : SV_Target
 {
-	float3 lightPos={0.0f,0.0f,-2.1f};
+	//float3 lightPos={0.0f,0.0f,-2.1f};
 	float3 s = txDiffuse.Sample(sampAni,input.Texcoord).xyz;
-	float3 normal = normalize(input.Normal.xyz);
+	/*float3 normal = normalize(input.Normal.xyz);
 	float3 lightVector = normalize(lightPos-input.worldPos.xyz);
 	float angle = clamp(dot(normal, lightVector),0,1);
-	float4 finalLight = float4(s*angle, 1.0f);
+	float4 finalLight = float4(s*angle, 1.0f);*/
 
-	return finalLight;
+	return float4(s, 1.0f);
 
 //	return input.Normal;
 	//return float4(input.Normal.xyz,1.0f);
