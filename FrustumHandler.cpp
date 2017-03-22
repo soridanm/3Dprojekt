84f5ae67c648654e8cafb6c46c875130b22fec67
soridanm <peter.meunier@kivling.com>
2017-03-22 17:55:54 +0100

    Last commit stopped to work, this one does.

--- FrustumHandler.cpp
+++ FrustumHandler.cpp
@@ -53,44 +53,6 @@ FrustumHandler::FrustumHandler(DirectX::XMFLOAT4X4 projection, DirectX::XMFLOAT4
 	planes[5].d = matrix._44 + matrix._42;
 
 
-
-	////near plane
-	//planes[0].a =-( matrix._14 + matrix._13);
-	//planes[0].b = -(matrix._24 + matrix._23);
-	//planes[0].c = -(matrix._34 + matrix._33);
-	//planes[0].d = -(matrix._44 + matrix._43);
-
-	////far plane
-	//planes[1].a = -(matrix._14 - matrix._13);
-	//planes[1].b = -(matrix._24 - matrix._23);
-	//planes[1].c = -(matrix._34 - matrix._33);
-	//planes[1].d = -(matrix._44 - matrix._43);
-
-	////left plane
-	//planes[2].a = -(matrix._14 + matrix._11);
-	//planes[2].b = -(matrix._24 + matrix._21);
-	//planes[2].c = -(matrix._34 + matrix._31);
-	//planes[2].d = -(matrix._44 + matrix._41);
-
-	////right plane
-	//planes[3].a = -(matrix._14 - matrix._11);
-	//planes[3].b = -(matrix._24 - matrix._21);
-	//planes[3].c = -(matrix._34 - matrix._31);
-	//planes[3].d = -(matrix._44 - matrix._41);
-
-	////top plane
-	//planes[4].a = -(matrix._14 - matrix._12);
-	//planes[4].b = -(matrix._24 - matrix._22);
-	//planes[4].c = -(matrix._34 - matrix._32);
-	//planes[4].d = -(matrix._44 - matrix._42);
-	////have to normalize
-
-	////bottom plane
-	//planes[5].a = -(matrix._14 + matrix._12);
-	//planes[5].b = -(matrix._24 + matrix._22);
-	//planes[5].c = -(matrix._34 + matrix._32);
-	//planes[5].d = -(matrix._44 + matrix._42);
-
 	//normalize all the planes
 	for (int i = 0; i < 6; i++) {
 		float length = sqrt((planes[i].a*planes[i].a) + (planes[i].b*planes[i].b) + (planes[i].c*planes[i].c));
@@ -153,37 +115,11 @@ bool FrustumHandler::checkVisible(DirectX::XMVECTOR boxMin, DirectX::XMVECTOR bo
 	//	for (int j = 0; j < 8; j++) {
 	//		//float temp = DirectX::XMVectorGetX(DirectX::XMVector3Dot(plane,diagonal[j] ));
 	//		float temp = DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[j]));
-
 	//		if (temp  <dot) {
 	//			diagonalCheck = j;
 	//			dot = temp;
 	//		}
 	//	}
-	//	//if (diagonalCheck == 0 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[0])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 1 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[1])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 2 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[2])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 3 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[3])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 4 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[4])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 5 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[5])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 6 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[6])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-	//	//if (diagonalCheck == 7 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, diagonal[7])) < 0) {
-	//	//	isVisible = false;
-	//	//}
-
 	//	if (diagonalCheck == 0 && DirectX::XMVectorGetX(DirectX::XMPlaneDot(plane, corner1)) < 0) {
 	//		isVisible = false;
 	//	}
