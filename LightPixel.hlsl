52b5cee572a51c01cb434ef7d9d3aed7015a45a9
viktor4006094 <viktor.enfeldt@gmail.com>
2017-04-08 22:02:20 +0200

    began to split the Light pass pixel shader into multiple functions

--- LightPixel.hlsl
+++ LightPixel.hlsl
@@ -34,7 +34,7 @@
 #ifdef SHADOW_MAP_SIZE
 static const float inverseShadowMapSize = rcp(SHADOW_MAP_SIZE);
 #else 
-static const float inverseShadowMapSize = 1.0;
+static const float inverseShadowMapSize = 0.0;
 #endif
 
 
@@ -46,7 +46,6 @@ Texture2D<float4> SpecularTexture		: register(t3); // r-g-b-specularPower
 Texture2D<float>  ShadowMap				: register(t4); // depth
 
 SamplerComparisonState compSampler : register(s0);
-SamplerState textureSampler; // TODO: look up sampler settings
 
 cbuffer shadowMapMatrix			: register(b0)
 {
@@ -78,9 +77,43 @@ struct PS_IN
 };
 
 //TODO: comment
-float2 texOffset(int u, int v)
+float2 TexOffset(int u, int v)
 {
 	return float2(u, v) * inverseShadowMapSize;
+}
+
+// 16-tap (4x4 texel area) Percentage-Closer Filtering using a comparison sampler
+float ShadowCoefficient(float2 texCoord, float lightDepth)
+{
+	float shadow_sum;
+
+	for (float y = -1.5; y <= 1.5; y += 1.0)
+	{
+		for (float x = -1.5; x <= 1.5; x += 1.0)
+		{
+			shadow_sum += ShadowMap.SampleCmpLevelZero(compSampler, texCoord + TexOffset(x, y), lightDepth);
+		}
+	}
+	return shadow_sum *= 0.0625; // Division by 16;
+}
+
+
+float3 DiffuseComponent(float3 toLight, float3 normal, float3 diffuseColor, int i)
+{
+	return max(0, dot(toLight, normal)) * (diffuseColor * Lights[i].color);
+}
+
+float3 SpecularComponent(float3 toLight, float3 toCamera, float3 normal, float4 specularColor, int i)
+{
+	float coef = pow(saturate(dot(reflect(-toLight, normal), toCamera)), specularColor.a);
+	return coef * (specularColor.rgb * Lights[i].color);
+}
+
+float AttenuationFactor(float3 positionWS, int i)
+{
+	float light_dist = distance(Lights[i].light_positionWS.xyz, positionWS);
+	return rcp(Lights[i].constant_attenuation 
+		+ light_dist * (Lights[i].linear_attenuation + light_dist * Lights[i].quadratic_attenuation));
 }
 
 float4 PS_main ( PS_IN input ) : SV_Target
@@ -112,32 +145,12 @@ float4 PS_main ( PS_IN input ) : SV_Target
 	shadow_tex_coord.y = (-light_view_pos.y * rcp(light_view_pos.w) * 0.5) + 0.5;  // Dividing by 2.0 and adding 0.5 gives position as uv coordinates [0, 1]
 
 	// calculate the distance from the light to the texel (the z value in NDC)
-	//float light_depth_value = light_view_pos.z / light_view_pos.w;
-	float light_depth_value = light_view_pos.z * rcp(light_view_pos.w);
+	float light_depth_value = light_view_pos.z * rcp(light_view_pos.w) - bias;
 
-
-	//subtract the bias to avoid self-shadowing
-	light_depth_value -= bias;
-
-	// TODO: turn into a function
 	// Test if the point is outside the light's view frustum
 	bool outside_shadow_map = (saturate(shadow_tex_coord.y) != shadow_tex_coord.y && saturate(shadow_tex_coord.x) != shadow_tex_coord.x) ? true : false;
-	// 16-tap (4x4 texel area) Percentage-Closer Filtering using a comparison sampler
-	float shadow_sum = 0.0;
-	float x, y;
-	if (!outside_shadow_map)
-	{
-		for (y = -1.5; y <= 1.5; y += 1.0)
-		{
-			for (x = -1.5; x <= 1.5; x += 1.0)
-			{
-				shadow_sum += ShadowMap.SampleCmpLevelZero(compSampler, shadow_tex_coord + texOffset(x, y), light_depth_value);
-			}
-		}
-		shadow_sum *= 0.0625; // Division by 16
-	}
 	
-	float shadow_coefficient = (outside_shadow_map) ? 1.0 : shadow_sum;
+	float shadow_coefficient = (outside_shadow_map) ? 1.0 : ShadowCoefficient(shadow_tex_coord, light_depth_value);
 	// FOR DEBUGGING: Overwrites the Percentage-Closer Filtering
 	//shadow_coefficient = (!outside_shadow_map) ? ShadowMap.SampleCmpLevelZero(compSampler, shadow_tex_coord - texOffset(0.5, 0.5), light_depth_value) : 1.0;
 	//shadow_coefficient = (light_depth_value < ShadowMap.Sample(textureSampler, shadow_tex_coord).r) ? 1.0 : 0.0;
@@ -157,13 +170,16 @@ float4 PS_main ( PS_IN input ) : SV_Target
 
 			// Diffuse part
 			diffuse_coefficient = max(dot(to_light, normal), 0.0);
-			diffuse_component = diffuse_coefficient * diffuseColor * Lights[i].color;
+			//diffuse_component = diffuse_coefficient * diffuseColor * Lights[i].color;
+			diffuse_component = DiffuseComponent(to_light, normal, diffuseColor, i);
 
 			// Specular part
 			/* specular_coefficient equals 0.0 if the light is behind the surface */
 			//specular_coefficient = (diffuse_coefficient > 0.0) ? pow(max(dot(reflection, to_camera), 0.0), specularValues.a) : 0.0;
-			specular_coefficient = (diffuse_coefficient > 0.0) ? pow(saturate(dot(reflection, to_camera)), specularValues.a) : 0.0;
-			specular_component = specular_coefficient * (specularValues.rgb * Lights[i].color);
+			
+			//specular_coefficient = (diffuse_coefficient > 0.0) ? pow(saturate(dot(reflection, to_camera)), specularValues.a) : 0.0;
+			//specular_component = specular_coefficient * (specularValues.rgb * Lights[i].color);
+			specular_component = (diffuse_coefficient > 0.0) ? SpecularComponent(to_light, to_camera, normal, specularValues, i) : float3(0.0, 0.0, 0.0);
 
 			// rcp(x) = (1.0 / x)
 			attenuation = rcp(Lights[i].constant_attenuation + distance_to_light * (Lights[i].linear_attenuation + distance_to_light * Lights[i].quadratic_attenuation));
